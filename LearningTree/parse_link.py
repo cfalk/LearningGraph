ca4c17738d9caf884357c022924e51bfb54af1b0
fig <fig@ironwood.haverford.edu>
2014-01-24 23:54:42 -0500

    Moar stuff

--- LearningTree/parse_link.py
+++ LearningTree/parse_link.py
@@ -1,14 +1,16 @@
 import re
 def parse_for_links(text):
-	return re.sub('\[.*?\]', link_replace, text)
+	def link_replace(matchobj):
+		link = matchobj.group(1)
+		text = matchobj.group(2) if len(matchobj.groups) == 3 else matchobj.group(1)
+
+		try:
+			linkObject = Link(url=link)
+			linkObject.save()
+		except:
+			pass 
+		return "<a href='{0}'>{1}</a>".format(link, text)
+
+	return re.sub('\[(.*?)\]\((.*?)\)', link_replace, text).sub('\[(.*?)\]', link_replace, text)
 
 
-
-def link_replace(matchobj):
-	link = matchobj.group()[1:-1]
-	print link
-	# TODO: check if link is in list
-	# TODO: if not, add a new link
-	# replace the link with a local URL
-	return "<a href='{0}'>{0}</a>".format(link)
-	
--- LearningTree/views.py
+++ LearningTree/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, redirect
 from django.contrib.auth import *
+from django.template import RequestContext
 
 from models import *
 from forms import *
 from db_constructors import *
-
+from retrievalFuncs import *
 
 def home(request):
  return render(request, "index.html")
@@ -15,12 +16,15 @@ def node(request):
 
 def node_form(request):
  u = request.user
-
+ 
  if u.is_authenticated() and request.method=="POST":
-  form = NodeForm(request.POST) 
+  node= Node()
+  node.user = u
+  form = NodeForm(request.POST, instance=node) 
+
   if form.is_valid():
-   form.user = u
    form.save()
+  return HttpResponse("Node created!")
  else:
   form = NodeForm() 
 
@@ -28,5 +32,14 @@ def node_form(request):
  
 def login(request):
  if request.user.is_authenticated():
-  return redirect('home')
+  return redirect('/index/')
  return render_to_response('login.html', {}, RequestContext(request))
+
+def user_nodes(request):
+ u= request.user
+ if u.is_authenticated():
+  nodes = get_user_nodes(u) 
+ else:
+  nodes = None
+ return render(request, "user_nodes.html", {"nodes": nodes})
+
