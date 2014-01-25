41de566117d4943527eadc1f0973b13bb96fa9b5
fig <fig@ironwood.haverford.edu>
2014-01-24 20:21:28 -0500

    third commit

--- LearningTree/views.py
+++ LearningTree/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from models.py import *
+from models import *
 
 def home(request):
  return render(request, "index.html")
