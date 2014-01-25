3c947027699f619d3bde923cfed39275e4ef2d5c
fig <fig@ironwood.haverford.edu>
2014-01-24 21:29:30 -0500

    More...

--- LearningTree/db_constructors.py
+++ LearningTree/db_constructors.py
@@ -1,15 +1,12 @@
 from models import *
 
-def new_node(name, user, content, related): #related is a list of nodes that will be connected to this node by edges
+def new_node(user, name, content, related): #related is a list of nodes that will be connected to this node by edges
  n = Node()
  n.name = name
  n.user = user
  n.content = content
  n.save()
  for node in related:
-  e = Edge()
-  e.node1 = n
-  e.node2 = node
+  e = Edge(node1=n, node2=node)
   e.save()
-
 
--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,1 +1,10 @@
-$("#kwicks-menu").kwicks(beahvior='menu');
+$('#kwicks-menu').kwicks({ 
+  event : 'click', 
+  eventClose: 'click', 
+  max : 330, 
+  spacing : 5, 
+  completed : function(){ 
+    addMessage('<span class="hilight">something, something,' + 
+      'something... com-plete</span>'); 
+  } 
+});
